e7014cfe6e67f9e626b121768dfb60c229c9ce5e
张振华 <2593481502@qq.com>
2017-08-21 08:43:13 +0800

    no message

--- app/Http/Controllers/Admin/WithdrawController.php
+++ app/Http/Controllers/Admin/WithdrawController.php
@@ -2,28 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Model\Withdraw;
 use Illuminate\Http\Request;
-use App\Http\Controllers\PublicController as Controller;
+use App\Http\Controllers\Controller;
 
 class WithdrawController extends Controller
 {
-    protected $withdraw;
-
-    public function __construct(Withdraw $withdraw)
+    public function cashList()//提现列表
     {
-        $this->withdraw=$withdraw;
-    }
 
-    public function cashList(Request $request)//提现列表
-    {
-        $query = Withdraw::query();
-        if($request->has('mobile'))
-            $query->where('mobile',$request->input('mobile'));
-        if($request->has('name'))
-            $query->where('name','like','%'.$request->input('name').'%');
-        $date=$query->paginate(config('admin.pages'));
-        $res=$this->paging($date);
-        return view('admin.withdraw.cashList',compact('date','res'));
     }
 }
