c779360ad1b1434bd1f5f78e9835f5732457ee49
配角 <1171119472@qq.com>
2017-08-22 14:40:25 +0800

    商城首页

--- routes/home/index.php
+++ routes/home/index.php
@@ -1,8 +1,8 @@
 <?php
-
-Route::get('index','UserController@index');//用户主页
-
-
-//var_dump(1111);die;
-
+Route::group(['namespace'=>'Home'],function(){
+    Route::get('register/index','LoginController@index');//加载注册页面
+    Route::get('register/agreement','LoginController@agreement');//加载用户注册协议页面
+    Route::post('register/goRegister','LoginController@goRegister');//添加去注册
+    Route::get('login/login','LoginController@login');//加载登录页面
+});
 ?>
