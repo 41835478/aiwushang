25995dd0f8cd993857c2c8fdcc46b6d8c4559fab
张振华 <2593481502@qq.com>
2017-08-22 17:59:18 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ require_once base_path('routes/admin/login.php');//后台登录退出
 
 
 //前台路由
-Route::group(['namespace'=>'Home','middleware'=>'home'],function(){
+Route::group(['namespace'=>'Home','middleware'=>'home.auth'],function(){
     require_once base_path('routes/home/user.php'); 
 });
 require_once base_path('routes/home/login.php');//前台注册、登录
