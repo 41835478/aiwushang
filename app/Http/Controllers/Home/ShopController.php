fbd9644b36bf228b70e18ccf0e46a0b48b3e7bac
配角 <1171119472@qq.com>
2017-08-23 20:31:35 +0800

    购物车进行中

--- app/Http/Controllers/Home/ShopController.php
+++ app/Http/Controllers/Home/ShopController.php
@@ -5,8 +5,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 
 class ShopController extends Controller{
+    const USER_ID = 'user_id';
     const LUNBO = 'advertisement';
     const GOODS = 'goods';
+    const CART = 'goodscart';
     //首页
     public function index(){
         $lunbo = DB::table(self::LUNBO)->where(['type'=>1,'status'=>0])->get();    //轮播图
@@ -22,10 +24,73 @@ class ShopController extends Controller{
     public function goodsDetail(){
         $id = $_REQUEST['id'];
         $goods = DB::table(self::GOODS)->where(['id'=>$id])->first();
+        $goods->small_pic = trim($goods->small_pic,']"');
+        $goods->small_pic = trim($goods->small_pic,'"[');
+        $goods->small_pic = explode('","',$goods->small_pic);
+//        var_dump($goods);
         $return = [
             'goods'=>$goods
         ];
         return view('home.shop.goodsDetail',$return);
+    }
+
+    //购物车
+    public function cart(){
+        session(['user_id'=>1]);
+        $user_id = session(self::USER_ID);
+        $cart = DB::table(self::CART)->where(['user_id'=>$user_id])->get()->toArray();
+        foreach($cart as $k=>$v){
+            $cart[$k]->goods = DB::table(self::GOODS)->where(['id'=>$v->goods_id])->first();
+        }
+        $return = [
+            'cart'=>$cart
+        ];
+        return view('home.shop.cart',$return);
+    }
+
+    //加入购物车
+    public function addCart(){
+        session(['user_id'=>1]);
+        $insert['num'] = $_REQUEST['num'];
+        $insert['goods_id'] = $_REQUEST['goods_id'];
+        $insert['user_id'] = session(self::USER_ID);
+        $insert['create_at'] = time();
+        $goods = DB::table(self::GOODS)->where(['id'=>$insert['goods_id']])->first();
+        if(in_array($goods->type,[4,5,6])){
+            $insert['type'] = 1;
+        }else{
+            $insert['type'] = 2;
+        }
+        $insert['price'] = round($insert['num'] * $goods->price,2);
+        $res = DB::table(self::CART)->where(['user_id'=>$insert['user_id'],'goods_id'=>$insert['goods_id']])->first();
+        if($res){
+            $update['num'] = $res->num + $insert['num'];
+            $update['price'] = $res->price + $insert['price'];
+            $yn = DB::table(self::CART)->where(['id'=>$res->id])->update($update);
+        }else{
+            $yn = DB::table(self::CART)->insert($insert);
+        }
+
+        if($yn){
+            echo 1;
+        }else{
+            echo 2;
+        }
+    }
+
+    //修改购物车数量
+    public function cartEdit(){
+        $update['num'] = $_REQUEST['num'];
+        $id = $_REQUEST['id'];
+        DB::table(self::CART)->where(['id'=>$id])->update($update);
+        echo 1;
+    }
+
+    //删除购物车
+    public function cartDel(){
+        $all_id = explode(',',trim($_REQUEST['xz_arr'],','));
+        $res = DB::table(self::CART)->whereIn('id',$all_id)->delete();
+        echo $res;
     }
 }
 
