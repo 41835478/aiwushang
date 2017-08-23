32521e93d03df94e23f0195624b166bfd3a4a160
mapengcheng <495285237@qq.com>
2017-08-23 10:44:33 +0800

    文章页

---
+++ resources/views/home/info/BeginnerGuide.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>爱无尚</title>
+<meta charset="utf-8"/>
+<meta name="author" content="jbs"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+<meta http-equiv="Content-Type" content="text/html; charset=GBK"/>
+<meta name="apple-mobile-web-app-capable" content="yes"/>
+<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+<meta name="format-detection" content="telephone=no"/>
+<meta http-equiv="Expires" content="-1"/>
+<meta http-equiv="Cache-Control" content="no-cache"/>
+<meta http-equiv="Pragma" content="no-cache"/>
+<meta name="description" content="" />
+<meta name="Keywords" content="" />
+<link rel="stylesheet" href="css/swiper.min.css"/>
+<link rel="stylesheet" type="text/css" href="font/iconfont.css"/>
+<link rel="stylesheet" href="css/common.css"/>
+
+<link rel="stylesheet" href="css/main.css">
+<script type="text/javascript" src="js/swiper.min.js"></script>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<style>
+	body{
+		background-color:#f5f5f5;
+	}
+</style>
+</head>
+<body>
+<div class="public_head">
+	<h3>新手必看</h3>
+	<a href="javascript:history.go(-1);" class="iconfont icon-fanhui"></a>
+</div>
+<!-- 内容区 -->
+<div class="content">
+	<ul class="Beginguide_ul">
+		<!-- <li class="BG_li">
+			<a href="javascript:void(0);">
+				<span>注册规则</span>
+				<i class="iconfont icon-you"></i>
+			</a>
+		</li> -->
+	</ul>
+</div>
+
+<script>
+	for(var i=0; i<3; i++){
+		var _li =   '<li class="BG_li">'
+				+		'<a href="registerRule.html">'
+				+			'<span>注册规则</span>'
+				+			'<i class="iconfont icon-you"></i>'
+				+		'</a>'
+				+	'</li>';
+		$(".Beginguide_ul").append(_li);
+	}
+</script>
+</body>
+</html>
---
+++ resources/views/home/info/registerRule.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>爱无尚</title>
+<meta charset="utf-8"/>
+<meta name="author" content="jbs"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+<meta http-equiv="Content-Type" content="text/html; charset=GBK"/>
+<meta name="apple-mobile-web-app-capable" content="yes"/>
+<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+<meta name="format-detection" content="telephone=no"/>
+<meta http-equiv="Expires" content="-1"/>
+<meta http-equiv="Cache-Control" content="no-cache"/>
+<meta http-equiv="Pragma" content="no-cache"/>
+<meta name="description" content="" />
+<meta name="Keywords" content="" />
+<link rel="stylesheet" href="css/swiper.min.css"/>
+<link rel="stylesheet" type="text/css" href="font/iconfont.css"/>
+<link rel="stylesheet" href="css/common.css"/>
+
+<link rel="stylesheet" href="css/main.css">
+<script type="text/javascript" src="js/swiper.min.js"></script>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<style>
+	body{
+		background-color:#fff;
+	}
+</style>
+</head>
+<body>
+<div class="public_head">
+	<h3>注册规则</h3>
+	<a href="javascript:history.go(-1);" class="iconfont icon-fanhui"></a>
+</div>
+<!-- 内容区 -->
+<div class="content pjt_content">
+	<div class="fenge"></div>
+	<div class="project_body">
+		<p>一个微信和电话注册一个帐号（同一帐号可以有无数个点位，达到要求方可提现）</p>
+		<p>（1）识别推荐人二维码就终身锁定绑定直推关系，登录进去浏览，点击购买、个人中心时提示先注册登录。</p>
+		<p>（2）注册完善资料提交以后，转跳到首页，购买商品送全国公排，有3种商品选择：</p>
+		<p>1.100元区商品；</p>
+		<p>2.300元区商品；</p>
+		<p>3.2000元区商品；</p>
+	</div>
+</div>
+</body>
+</html>
